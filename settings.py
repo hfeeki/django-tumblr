2bc6564eb1611a26cb202484c7beaa92114798b3
Will Van Wazer <will@willvanwazer.com>
2011-06-22 11:15:51 -0400

    Added basic quote template, fixed settings file for new location.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/Will/Documents/Work_Space/Django_Projects/tumblog/database.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/Will/Code/sideprojects/tumblog/database.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'tumblog.urls'
 
 TEMPLATE_DIRS = (
-	"/Users/Will/Documents/Work_Space/Django_Projects/tumblog/templates"
+	"/Users/Will/Code/sideprojects/tumblog/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
