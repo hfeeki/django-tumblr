aacbb708fedbf2108afa2d92a3e97090ef4392f7
Will Van Wazer <will@willvanwazer.com>
2011-10-19 23:52:45 -0400

    MDST template and midterm.

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import direct_to_template
+
 
 admin.autodiscover()
 
@@ -10,6 +12,7 @@ urlpatterns = patterns('',
     url(r'^$', 'django_tumblog_proj.apps.tumblog.entries.views.homepage', name='homepage'),
     url(r'^blog/', include('django_tumblog_proj.apps.tumblog.urls')),
     url(r'^page/(\S+)/$', 'django_tumblog_proj.apps.pages.views.singlepage'),
+	url(r'^mdst-midterm/$', direct_to_template, {'template': 'mdst_midterm.html'}),
 )
 # 
 # urlpatterns += staticfiles_urlpatterns()
